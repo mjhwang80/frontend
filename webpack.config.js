8e0b4cab02d420c83439aa03ad51ee477200f02f
mjhwang80 <hmjkor@gmail.com>
2023-02-13 17:02:22 +0900

    test

--- webpack.config.js
+++ webpack.config.js
@@ -40,13 +40,23 @@ module.exports = {
     ],
   },
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
         publicPath: '/',
       },
       optimization: {
+        moduleIds: 'deterministic',
         runtimeChunk: 'single',
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all',
+            },
+          },
+        },
       },
     
   };
